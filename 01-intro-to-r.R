6df9712c52a63313e4a46dce60d3e1d1a870df6e
Peter Kiraly <pkiraly@gwdu100.gwdg.de>
2020-11-26 14:08:16 +0100

    Improving intro

--- 01-intro-to-r.R
+++ 01-intro-to-r.R
@@ -1,1 +1,21 @@
-01-intro-to-r
+#' Creating objects in R
+3 + 5
+12 / 7
+
+# assign values to objects
+weight_kg <- 55
+
+#' key for "<-"
+#' win, linux: Alt + -
+#' mac: Option + -
+
+#' naming conventions
+#' - be explicit and not too long
+#' - cannot start with a number (2x is not valid, but x2 is)
+#' - case sensitive (weight_kg != Weight_kg)
+#' - do not use reserved names (e.g., if, else, for ...). 
+#' - do not use other function names
+#' - avoid dots (.)
+#' - use nouns for object names
+#' - use verbs for function names
+#' - be consistent in the styling of your code
