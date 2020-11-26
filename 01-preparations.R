0dfb545a58eadf2dc6fb89248bb8d0dfee21efe5
Peter Kiraly <pkiraly@gwdu100.gwdg.de>
2020-11-26 11:31:42 +0100

    Initialize repository

---
+++ 01-preparations.R
@@ -0,0 +1,8 @@
+#' Install necessary packages (which provide additional functionality to the core)
+install.packages(c("tidyverse", "hexbin", "patchwork", "RSQLite"))
+
+#' import them into the code
+library(tidyverse)
+library(hexbin)
+library(patchwork)
+library(RSQLite)
---
+++ 02-before-we-start.R
@@ -0,0 +1,54 @@
+#' R
+#' - the programming language
+#' - the software that interprets the scripts written in that language
+#' 
+#' RStudio
+#' - to write your R scripts
+#' - to interact with the R software
+#' 
+#' Why learn R?
+#' - learning curve might be steeper
+#' - do not rely on remembering a succession of pointing and clicking,
+#'   but instead on a series of written commands
+#' - the code can be inspected by someone else (for feedback or reproducibility)
+#' 
+#' Reproducibility
+#' - someone else (including your future self) can obtain the same results 
+#'   from the same dataset when using the same analysis.
+#' - requirement of journals and funding agencies
+#' 
+#' interdisciplinary and extensible
+#' - 10,000+ packages
+#' - many scientific disciplines
+#' 
+#' data of all shapes and sizes
+#' - scalable data size
+#' - handling of missing data and statistical factors
+#' - connect to spreadsheets, databases, other data formats
+#' 
+#' and
+#' - high-quality graphics
+#' - community
+#' - free, open-source and cross-platform
+#' 
+#' 4 "Panes"
+#' - Source for your scripts and documents (top-left),
+#' - Environment/History (top-right) 
+#'   - shows all the objects in your working space (Environment)
+#'   - command history (History)
+#' - Files/Plots/Packages/Help/Viewer (bottom-right)
+#' - R Console (bottom-left) 
+#' - shortcuts, autocompletion, and highlighting
+
+#' Getting set up
+#' 1. File > New Project > Choose New Directory > New Project
+#' 2. Enter a name for this new folder, choose a location. 
+#'    (this will be the "working directory" e.g., ~/data-carpentry.
+#' 3. Create Project
+#' 4. Download code: https://datacarpentry.org/R-ecology-lesson/code-handout.R
+#'    place it in working directory and rename it (e.g., data-carpentry-script.R)
+url <- 'https://datacarpentry.org/R-ecology-lesson/code-handout.R'
+destfile <- 'data-carpentry-script.R'
+download.file(url, destfile)
+#' 5. (Optional) Set Preferences to 'Never' save workspace in RStudio.
+#' 
